cdfe44758dbc66356d4df4b25ea0305ec33b7f09
Roberto Costa <roberto.araujo339@gmail.com>
2023-10-24 17:31:04 -0300

    aws security group config

---
+++ infra/security-groups.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "general" {
+  name = "general"
+  description = "Allow all traffic"
+  ingress{
+    cidr_blocks = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+  egress{
+    cidr_blocks = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+  tags = {
+    Name = "tests"
+  }
+}
